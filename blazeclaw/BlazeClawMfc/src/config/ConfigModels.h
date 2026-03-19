bd845ac10b24b25caa30929b8b4f2d89c46e990c
Jicheng Hu <jicheng@yahoo.com>
2026-03-19 10:25:38 +0800

    Update config defaults and add blazeclaw.conf file
    Change GatewayConfig
    default port to 56789, and add blazeclaw.conf with gateway,
    agent, and channel settings.

--- blazeclaw/BlazeClawMfc/src/config/ConfigModels.h
+++ blazeclaw/BlazeClawMfc/src/config/ConfigModels.h
@@ -6,7 +6,7 @@ namespace blazeclaw::config {
 
 struct GatewayConfig {
   std::wstring bindAddress = L"127.0.0.1";
-  std::uint16_t port = 18789;
+  std::uint16_t port = 56789;
 };
 
 struct AgentConfig {
